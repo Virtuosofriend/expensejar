6c7ea37a7ff718b407c4989b5bf65fe842c05249
Dimitris Sakellariou <dimitris.sakella@gmail.com>
2022-01-29 12:01:16 +0200

    Main JS fixes for linter rules

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from "vue"
-import App from "./App.vue"
-import router from "./router"
-import store from "./store"
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 import i18n from "./plugins/i18n";
 import vuetify from "./plugins/vuetify"
 import "@/plugins/dayjs";
 import HighchartsVue from "highcharts-vue";
-import { firestorePlugin } from "vuefire"
+import { firestorePlugin } from "vuefire";
 
 Vue.use(HighchartsVue);
 Vue.use(firestorePlugin);
@@ -15,22 +15,23 @@ Vue.config.productionTip = false;
 * Globally register all `_base`-prefixed components
 */
 
-import "./plugins/base.js"
-import "./helpers/backbutton.js"
+import "./plugins/base.js";
+import "./helpers/backbutton.js";
 
 /**
 * Globally register filters
 */
-import OnlyMonth from "@/common/onlyMonthdate.filter.js"
-import ShortMonthDay from "@/common/shortMonthDaydate.filter.js"
+import OnlyMonth from "@/common/onlyMonthdate.filter.js";
+import ShortMonthDay from "@/common/shortMonthDaydate.filter.js";
 
 Vue.filter("onlyMonth", OnlyMonth);
 Vue.filter("ShortMonthDay", ShortMonthDay);
 
 new Vue({
+    name: "ExpenseJar",
 	router,
 	store,
 	vuetify,
     i18n,
 	render: (h) => h(App),
-}).$mount("#app")
+}).$mount("#app");

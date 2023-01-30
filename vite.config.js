91aa693237f0ae1f2727331944c6b27b1f03436e
Dimitris Sakellariou <dimitris.sakella@gmail.com>
2023-01-30 17:25:37 +0200

    EJ-16: Add category transportation icon

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { fileURLToPath, URL } from "node:url"
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { VantResolver } from "unplugin-vue-components/resolvers";
 
